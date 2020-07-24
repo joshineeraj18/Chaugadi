d3b7a36e2c1ad35a422929e7c300350fac734a18
Neeraj <bhairaviwellbeing@gmail.com>
2020-07-24 18:22:02 +0530

    --Added feature of forget password activity

--- app/src/main/java/com/bhairaviwellbeing/chaugadi/ForgetPasswordActivity.java
+++ app/src/main/java/com/bhairaviwellbeing/chaugadi/ForgetPasswordActivity.java
@@ -1,16 +1,70 @@
 package com.bhairaviwellbeing.chaugadi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class ForgetPasswordActivity extends AppCompatActivity {
+
+    EditText send_email;
+    Button btn_reset;
+
+    FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Reset Password");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        send_email = findViewById(R.id.send_email);
+        btn_reset = findViewById(R.id.btn_reset);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = send_email.getText().toString();
+
+                if(email.equals("")){
+                    Toast.makeText(ForgetPasswordActivity.this,"Email ID can not be left blank ",Toast.LENGTH_SHORT).show();
+                }else {
+                    firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(ForgetPasswordActivity.this,"Email Sent!",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(ForgetPasswordActivity.this,LoginActivity.class));
+                            }else {
+                                String erroe = task.getException().getMessage();
+                                Toast.makeText(ForgetPasswordActivity.this,erroe,Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
 
 
 
